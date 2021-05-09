4c444ae5143d579b2408771937d8a7e742c65c73
JeongkyuKim-git <66001539+JeongkyuKim-git@users.noreply.github.com>
2021-05-09 14:59:06 +0900

    Update numpy_basic.py

--- Linear_algrbra/numpy_basic.py
+++ Linear_algrbra/numpy_basic.py
@@ -1,1 +1,23 @@
-dddd
+import numpy as np
+
+A = np. array([
+    [1,-1,2],
+    [3,2,2],
+    [4,1,2],
+    [7,5,6]
+    ])
+    
+print(A)
+print("")
+
+# zeros는 () 괄호를 한 번 사용해야함
+C = np.random.rand(3,5)
+print(C)
+print("")
+
+# zeros는 () 괄호를 한 번더 사용해야함
+D = np.zeros((2,4))
+print(D)
+print("")
+
+print(A[0][0])
